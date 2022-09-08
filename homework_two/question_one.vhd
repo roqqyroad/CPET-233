e68565104ebea995bbf29b072f3076590bbe890d
Roquel <rachel@duboishouse.com>
2022-09-08 17:22:44 -0400

    Updated to reflect final code for Homework Two.

--- homework_two/question_one.vhd
+++ homework_two/question_one.vhd
@@ -11,7 +11,7 @@ entity question_one is
         a1, a0, b1, b0 : in std_logic;
 
         --output
-        gt : out std_logic;
+        gt : out std_logic
     );
 end question_one;
 
@@ -21,6 +21,9 @@ begin
     
     --PART C; unsimplified equation
     --GT = !A1A0!B1!B0 + A1!A0!B1!B0 + A1!A0!B1B0 + A1A0!B1!B0 + A1A0!B1B0 + A1A0B1!B0
-    gt <= ((NOT a1 AND a0 AND NOT b1 AND NOT b0) OR (A1 AND NOT a0 AND NOT b1 AND NOT b0) OR (a1 AND NOT a0 AND NOT b1 AND b0) OR (a1 AND a0 AND NOT b1 AND NOT b0) OR (a1 AND a0 AND NOT b1 AND b0) OR (a1 AND a0 AND b1 AND NOT b0));
+    --gt <= ((NOT a1 AND a0 AND NOT b1 AND NOT b0) OR (A1 AND NOT a0 AND NOT b1 AND NOT b0) OR (a1 AND NOT a0 AND NOT b1 AND b0) OR (a1 AND a0 AND NOT b1 AND NOT b0) OR (a1 AND a0 AND NOT b1 AND b0) OR (a1 AND a0 AND b1 AND NOT b0));
 
+	 --Part g; putting in the new equation from the compilation
+	 gt <= ((NOT a1 AND (NOT b1 AND (a0 AND NOT b0))) OR (a1 AND ((NOT b1) OR (a0 AND NOT b0))));
+	 
 end model;
