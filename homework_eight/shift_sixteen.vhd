8ead0810c3c995e3e3f37b19e8a3156b86a7e2cb
Rachel DuBois <rachel@duboishouse.com>
2022-11-24 20:52:21 -0500

    finished 16 bit universal shifter assignment part

--- homework_eight/shift_sixteen.vhd
+++ homework_eight/shift_sixteen.vhd
@@ -1,7 +1,8 @@
 --NAme: Rachel DuBois
 --Section: 02
 --Lab: 02
---Assignment: Homework Eight
+--Assignment: Homework Eight 
+-- Summary: 16 bit universal shift register
 
 
 --libraries
@@ -14,9 +15,11 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity shift_sixteen is
     port(
         --inputs
-        clk, reset_n, sh_ld, r_l, serial_in : in std_logic;
-		  --outputs
-		  serial_out : out std_logic
+        clk, reset_n, sh_ld, r_l, serial_in: in std_logic;
+		din : in std_logic_vector(15 downto 0); --for parallel loading
+		--outputs
+		serial_out : out std_logic;
+		all_out : out std_logic_vector(15 downto 0)
     );
 end shift_sixteen;
 --end of entity
@@ -24,49 +27,44 @@ end shift_sixteen;
 --architecture
 architecture model of shift_sixteen is
 	--signals
-	signal int_count : std_logic_vector(3 downto 0);
-	
-	--constants
-	constant max_val : std_logic_vector(3 downto 0) := "1111";
-	constant min_val : std_logic_vector(3 downto 0) := "0000";
+	signal shifting : std_logic_vector(15 downto 0);
+	signal s_serial_out : std_logic;
+
 begin
 
 	--counting if processes
-	counting : process(clk, reset_n, u_d)
+	counting : process(clk, reset_n, sh_ld, r_l)
 	begin
 		
-		--reset is active low, if reset, intcount is zeros
+		--reset is active low, 
 		if(reset_n = '0') then
-			int_count <= "0000";
+			shifting <= "000000000000000"; --shifting resets to all zero's
 		
 		--if there is a rising clock edge then
 		elsif(rising_edge(clk)) then
 		
-			--if ud is one we are supposed to count up
-			if(u_d = '1') then
-			
-				--but if its already at the max value it stays at the value it is at
-				if (int_count = max_val) then
-					int_count <= int_count;
-				--otherwise it has one added to it 
-				else
-				int_count <= int_count + 1;
-				end if;
-			--BUT IF UD IS ZERO we are supposed to count down
-			elsif(u_d = '0') then
-				--so if intcount is equal to zero it stays the same
-				if(int_count = min_val) then
-					int_count <= int_count;
-				--else it gets one subtracted from it
-				else
-				int_count <= int_count - 1;
+			--PARALLEL LOAD
+			if(sh_ld = '0') then
+				shifting <= din; --PARALLEL LOADING for shifting (lack there of, rather)
+
+			elsif(sh_ld = '1') then --SHIFTING for shifting (actually shifting)
+
+				if(r_l = '0') then--LEFT i think...
+					shifting(0 to 14) <= shifting(1 to 15); --SHIFT left
+					shifting(15) <= serial_in;
+					s_serial_out <= shifting(15); --serial out gets 15 place
+
+				else--RIGHT
+					shifting(1 to 15) <= shifting(0 to 14); --SHIFT RIGHT
+					shifting(0) <= serial_in;
+					s_serial_out <= shifting(0); --serial out gets 0 place
+
 				end if;
 			end if;
 		end if;
 	end process;
 	--end of processes
-	
-	--set output equal to the int_count value
-	count <= int_count;
+	serial_out <= s_serial_out; --serial out !
+	all_out <= shifting;--parallel output, just in case...
 end model;
 --end of architecture
